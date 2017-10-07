49dfbb8b2ccc03d51a6bb5c15abc73fc7850963d
Florent Sorel <florent@wixiweb.fr>
2017-10-07 22:59:58 +0200

    feat(header): ajout du composant

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
     <div id="app">
-        <header>
-            <span>Vue.js PWA</span>
-        </header>
+        <app-header></app-header>
         <main>
-            <img src="./assets/logo.png" alt="Vue.js PWA">
             <router-view></router-view>
         </main>
     </div>
 </template>
 
 <script>
+    import Header from './components/Header.vue'
     export default {
-        name: 'app'
+        name: 'app',
+        components: {
+            'app-header': Header
+        }
     }
 </script>
 
 <style>
     body {
+        display: flex;
         margin: 0;
     }
 
@@ -31,24 +33,5 @@
     main {
         text-align: center;
         margin-top: 40px;
-    }
-
-    header {
-        margin: 0;
-        height: 56px;
-        padding: 0 16px 0 24px;
-        background-color: #35495E;
-        color: #ffffff;
-    }
-
-    header span {
-        display: block;
-        position: relative;
-        font-size: 20px;
-        line-height: 1;
-        letter-spacing: .02em;
-        font-weight: 400;
-        box-sizing: border-box;
-        padding-top: 16px;
     }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <img src="../assets/habitat76.png" alt="Habitat 76" width="100" height="58">
+    </header>
+</template>
+
+<style lang="scss" scoped>
+    @import '../assets/sass/variables.scss';
+    @import "~bootstrap-sass/assets/stylesheets/bootstrap/mixins/gradients";
+    header {
+        position: relative;
+        display: flex;
+        height: 80px;
+        img {
+            margin: auto;
+        }
+        &:after {
+            content: " ";
+            display: block;
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: -7px;
+            height: 6px;
+            background: $primary-color;
+            @include gradient-horizontal($primary-color, $secondary-color, 20%);
+
+        }
+    }
+</style>
